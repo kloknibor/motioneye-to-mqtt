f78c02b695c8e122b7b1918dc273aed17b9df652
Robin Kolk <robinkolk@msn.com>
2019-11-07 22:04:30 +0100

    make settings changeable

--- motioneye-to-mqtt.py
+++ motioneye-to-mqtt.py
@@ -104,6 +104,16 @@ if __name__ == "__main__":
     for i in sys.argv:
         print(i)
 
+    if len(sys.argv) >= 3:
+        SMB_FOLDER = sys.argv[2]
+        print(sys.argv[2])
+    if len(sys.argv) >= 4:
+        MQTT_TOPIC_SNAPSHOT = sys.argv[3]
+        print(sys.argv[3])
+    if len(sys.argv) >= 5:
+        MQTT_TOPIC_MOTION = sys.argv[4]
+        print(sys.argv[4])
+
     try:
         # main program
         if sys.argv[1] == "ON":
